0736b6b2f8145690f72ce77bf11a089d7de84c61
Phil Lucks <philip.lucks@snapengage.com>
2019-01-05 00:07:41 -0700

    cleanup

--- src/scripts/components/features/graph/LabelAsPoint.js
+++ src/scripts/components/features/graph/LabelAsPoint.js
@@ -1,24 +1,19 @@
-import React, { Component } from "react";
+import React from "react";
 
-export default class LabelAsPoint extends React.Component {
-  onClick = e => {
-    debugger;
-    const { index, key, payload } = this.props;
-    // you can do anything with the key/payload
-  };
-  render() {
-    const { x, y, index } = this.props;
-    return (
-      <circle
-        className="circle-segment"
-        onClick={e => {
-          this.props.handleClick(e, index);
-        }}
-        cx={x}
-        cy={y}
-        r={8}
-        fill="transparent"
-      />
-    );
-  }
-}
+const LabelAsPoint = props => {
+  const { x, y, index } = props;
+  return (
+    <circle
+      className="circle-segment"
+      onClick={e => {
+        this.props.handleClick(e, index);
+      }}
+      cx={x}
+      cy={y}
+      r={8}
+      fill="transparent"
+    />
+  );
+};
+
+export default LabelAsPoint;
